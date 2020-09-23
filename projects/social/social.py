c2c938f9ab6ca87dc73dd4bbf6ae7f24b1aa4825
Eric <eric@erics-mbp.localdomain>
2020-09-22 22:30:54 -0500

    Completed social.py

--- projects/social/social.py
+++ projects/social/social.py
@@ -1,12 +1,34 @@
+import random
+import math
+
+
+class Queue():
+    def __init__(self):
+        self.queue = []
+
+    def enqueue(self, value):
+        self.queue.append(value)
+
+    def dequeue(self):
+        if self.size() > 0:
+            return self.queue.pop(0)
+        else:
+            return None
+
+    def size(self):
+        return len(self.queue)
+
+
 class User:
     def __init__(self, name):
         self.name = name
 
+
 class SocialGraph:
     def __init__(self):
         self.last_id = 0
-        self.users = {}
-        self.friendships = {}
+        self.users = {}  # {1: User("1"), 2: User("2"), ...}
+        self.friendships = {}  # {1: {2,3,4}, 2: {1}, 3: {1}, 4:{1}}
 
     def add_friendship(self, user_id, friend_id):
         """
@@ -42,23 +64,55 @@ class SocialGraph:
         self.last_id = 0
         self.users = {}
         self.friendships = {}
-        # !!!! IMPLEMENT ME
 
         # Add users
+        for i in range(num_users):
+            self.add_user(f"User {i}")
 
         # Create friendships
+        # Generate all the possible friendships and put them into an array
+        # 3 users (0,1,2)
+        # [(0,1), (0,2), (1,2)]
+        possible_friendships = []
+        for user_id in self.users:
+            # To prevent duplicate friendships create from user_id +1
+            for friend_id in range(user_id + 1, self.last_id + 1):
+                possible_friendships.append((user_id, friend_id))
+
+        # Shuffel the friendship array
+        # [(1,2),(0,1), (0,2)]
+        random.shuffle(possible_friendships)
+
+        # Take the first num_users * avg_friendships / 2 and that wil be the friendships for the graph
+        for i in range(math.floor(num_users * avg_friendships/2)):
+            friendship = possible_friendships[i]
+            self.add_friendship(friendship[0], friendship[1])
 
     def get_all_social_paths(self, user_id):
         """
         Takes a user's user_id as an argument
-
         Returns a dictionary containing every user in that user's
         extended network with the shortest friendship path between them.
-
         The key is the friend's ID and the value is the path.
         """
         visited = {}  # Note that this is a dictionary, not a set
-        # !!!! IMPLEMENT ME
+        neighbors_to_visit = Queue()
+        neighbors_to_visit.enqueue([user_id])
+        while neighbors_to_visit.size() > 0:
+            # deque the first path
+            current_path = neighbors_to_visit.dequeue()
+            # grab most recent vertex
+            current_vertex = current_path[-1]
+            # if the current vertex has not been visited
+            if current_vertex not in visited:
+                # if not visited[current_vertex] or current_path not in visited:
+                # add current vertex to the visited dict with
+                # path that led here
+                visited[current_vertex] = current_path
+                for n in self.friendships[current_vertex]:
+                    path_copy = current_path.copy()
+                    path_copy.append(n)
+                    neighbors_to_visit.enqueue(path_copy)
         return visited
 
 
